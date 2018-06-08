e9bea6d0e1f7ab78ca4240ff9b0ad82104c3c72d
Yiwei Liu <lywei.dinpay@gmail.com>
2018-06-08 21:01:30 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Quartz Cron 定时任务
+
+Steps:
+1. pom.xml dependencies
+quartz
+spring-boot-starter 
+
+2.QuartzApplication Annotation
+@EnableScheduling
+
+3.SimpleJob.class
+ 
---
+++ src/main/java/com/dinpay/bc/quartz/jobs/SimpleJob.java
@@ -0,0 +1,26 @@
+package com.dinpay.bc.quartz.jobs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleJob {
+
+    private final static Logger logger = LoggerFactory.getLogger(SimpleJob.class);
+
+    @Scheduled(cron = "0/2 * * * * ?")
+    public void handleTasks() {
+
+        logger.info("... start to handle batch jobs ...");
+
+        try {
+            //concurrent default false
+            Thread.currentThread().sleep(3000);
+        } catch (InterruptedException e) {
+            logger.error("error occurred: ", e);
+        }
+    }
+
+}
